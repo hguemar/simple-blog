e3d708639f879c626f7ae701089f4186450ba50d
Fall1ngStar <titom98@live.fr>
2019-06-05 16:20:41 +0200

    added bootstrap

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
-var express = require("express");
-var MongoClient = require("mongodb");
-var bodyParser = require('body-parser')
-var cons = require('consolidate');
+const express = require("express");
+const MongoClient = require("mongodb");
+const bodyParser = require('body-parser')
+const cons = require('consolidate');
+const path = require('path');
 
-var app = express();
-var url = process.env.URL || "mongodb://localhost:27017";
-var dbName = process.env.DBNAME || "test";
-var port = process.env.PORT || 8000;
+const app = express();
+const url = process.env.URL || "mongodb://localhost:27017";
+const dbName = process.env.DBNAME || "test";
+const port = process.env.PORT || 8000;
 
 app.engine('html', cons.pug);
 
 app.set('view engine', 'html');
-app.set('views',  __dirname +  '/views')
+app.set('views', path.join( __dirname,  '/views'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use('/static', express.static(path.join(__dirname, '/static')));
+
 
 var routes = require("./routes");
 
