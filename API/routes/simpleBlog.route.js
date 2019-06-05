a73b4c240cc0021a0b87de0ae24f1d998755b175
Lucas <martel.lucas069@gmail.com>
2019-06-05 16:30:10 +0200

    Génération des articles aléatoires

--- API/routes/simpleBlog.route.js
+++ API/routes/simpleBlog.route.js
@@ -1,8 +1,15 @@
 const express = require('express'),
     simpleBlogRoutes = express.Router();
+const dataConfig = require("../config/data");
 
 simpleBlogRoutes.all("/" , (req,res) => {
     res.send("Hello world");
+});
+
+
+simpleBlogRoutes.get("/generate", (req, res) => {
+    //TODO Remplir la base de données
+    res.send(dataConfig.articles[0]);
 });
 
 module.exports = simpleBlogRoutes;
