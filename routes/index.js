de5e329024b04561973171a13974d76579370669
SPONTAK ANTONIN p1501019 <p1501019@univ-lyon1.fr>
2019-06-06 11:55:30 +0200

    feat: Get all articles and new page to view the whole article

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,18 @@ var posts = require("./posts");
 
 module.exports = function(app) {
 
-  // GET ALL THE ARTICLES
-  var allPosts = {'posts': ['post1', 'post2', 'post3', 'post4']};
-
   app.get("/", function(req, res) {
-    res.render("index", allPosts);
-  });
+    
+    app.db.collection("article").find().toArray(function(err, result) {
+      if (err) throw err;
+      console.log({'articles': result});
+      res.render("index", {'articles': result});
+    });
 
-  app.get("/ajoutBlog", function(req, res) {
-    res.render("ajoutBlog", allPosts);
+    app.get("/ajoutBlog", function(req, res) {
+	  res.render("ajoutBlog", allPosts);
+    });
+
   });
 
   // Register posts endpoint
