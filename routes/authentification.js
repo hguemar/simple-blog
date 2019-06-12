fca59ac112d7086eeac6853cb1a25dc71ced8d15
TRAN Minh An <bornbygoogle@yahoo.com>
2019-06-12 19:57:25 +0200

    remove user blocked

--- routes/authentification.js
+++ routes/authentification.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 // Authentication and Authorization Middleware
 var auth = function(req, res, next) {
-	if (req.session && req.session.user == "rwieruch" && req.session.admin)
+	if (req.session && req.session.user != null && req.session.admin)
 	  return next();
 	else
 	  return res.sendStatus(401);
