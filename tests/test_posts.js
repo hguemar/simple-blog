4af796c146129095f1909f454b4ac1b97b934c4c
nosql <nosql@localhost.localdomain>
2019-06-14 23:32:42 +0200

    Ajout des tests

--- tests/test_posts.js
+++ tests/test_posts.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var app = require("../server.js");
 var supertest = require("supertest");
 var assert = require("chai").assert;
@@ -25,3 +26,53 @@ describe("test blog posts endpoint", function() {
     });
   })
 })
+=======
+var app = require("../server.js");
+var supertest = require("supertest");
+var assert = require("chai").assert;
+
+describe("test blog posts endpoint", function() {
+  describe("GET /post/create", function() {
+    it("should have content-type text/html", function(done) {
+      supertest(app)
+        .get("/post/create")
+        .set("User-Agent", "API testing")
+        .expect("Content-Type", "text/html; charset=utf-8")
+        .expect(200)
+        .end(done);
+    });
+    it("should have a form!", function(done) {
+      supertest(app)
+        .get("/post/create")
+        .set("User-Agent", "API testing")
+        .expect(function(res) {
+          console.log(res.text);
+          assert(res.text.search("<form>.*</form>") != -1);
+        })
+        .expect(200)
+        .end(done);
+    });
+  });
+  describe("GET /post/:id", function() {
+    it("wrong id", function(done) {
+      supertest(app)
+          .get("/post/")
+          .set("User-Agent", "API testing")
+          .expect(function(res) {
+            assert(res.text.insert("Not found"))
+          })
+          .expect(404)
+          .end(done);
+    });
+  });
+  describe("DELETE /post/suppr/:id", function() {
+    it("", function(done) {
+      supertest(app)
+          .get("/post/suppr/:id")
+          .set("User-Agent", "API testing")
+          .expect(200)
+          .end(done);
+    });
+  });
+});
+>>>>>>> b9641d85467e4e03234f9820ed8150717c23d03d
