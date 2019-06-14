b510a4f2ed53ef7bf8636f90f240d89aba9c0315
Lucas <martel.lucas069@gmail.com>
2019-06-14 22:01:30 +0200

    Ajout des opération de CRUD sur l'API

--- API/routes/simpleBlog.route.js
+++ API/routes/simpleBlog.route.js
@@ -14,13 +14,30 @@ simpleBlogRoutes.get("/generate", (req, res) => {
     dbUtil.insertDocument(config.database.collection, dataConfig.articles, res);
 });
 
+// Récupère tous les documents
 simpleBlogRoutes.get("/posts", (req, res) => {
     dbUtil.getAllDocument(config.database.collection, res);
 });
+// Récupère un seul document
 simpleBlogRoutes.get("/posts/:id", (req, res) => {
     const _id = req.params.id;
     console.log(_id);
     dbUtil.getDocumentByID(config.database.collection, _id, res);
+});
+
+// Update un seul document
+simpleBlogRoutes.put("/posts/:id", (req, res) => {
+    const _id = req.params.id;
+    dbUtil.findDocumentByIdAndUpdate(config.database.collection, _id, req.body, res);
+});
+simpleBlogRoutes.delete("/posts/:id", (req, res) => {
+    const _id = req.params.id;
+    console.log("delete");
+    dbUtil.findDocumentByIdAndDelete(config.database.collection, _id, res);
+});
+
+simpleBlogRoutes.post('/posts', (req, res) => {
+    dbUtil.insertDocument(config.database.collection, req.body, res);
 });
 
 module.exports = simpleBlogRoutes;
