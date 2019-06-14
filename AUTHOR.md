db20abda04cac3045397da30701afc023d16bf3c
loic.forest <loic.forest@hardis.fr>
2019-06-15 00:17:19 +0200

    First commit

---
+++ AUTHOR.md
@@ -0,0 +1,3 @@
+Loic Forest forestloic93@gmail.com ou loic.forest@etu.univ-lyon1.fr
+
+Minh An Tran bornbygoogle@yahoo.com ou minh-an.tran@etu.univ-lyon1.fr
---
+++ routes/tags.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', async (req, res) => {
+  const tags = await req.context.models.Tags.find();
+  return res.send(tags);
+});
+
+router.get('/:tagId', async (req, res) => 
+{
+	// Get all posts id with the given tagID
+	var tagsposts = await req.context.models.Tags.findById(req.params.tagId);
+	// Get all posts
+	var posts = await req.context.models.Posts.find({ _id : { $in : tagsposts.posts}}).populate('author tags').exec();
+
+	res.render('landing', { posts: posts, tag: tagsposts.tag, userID: req.session.userID });
+});
+
+router.post('/', async (req, res) => 
+{
+	const tag = await req.context.models.Tags.create(
+		{
+			tag: req.body.tag,
+		});
+
+	res.render('posts', post);
+});
+
+router.delete('/:postId', async (req, res) => {
+  const post = await req.context.models.Posts.findById(
+    req.params.postId,
+  );
+
+  let result = null;
+  if (post) {
+    result = await post.remove();
+  }
+
+  return res.send(result);
+});
+
+module.exports = router;
